4f216caca588ad92c3c35afc53481aadb0287dab
Gru <78332542+gru2007@users.noreply.github.com>
2025-10-06 01:00:23 +1000

    Update index.jsx

--- packages/frontend/src/pages/index.jsx
+++ packages/frontend/src/pages/index.jsx
@@ -4,17 +4,22 @@ import { FaBrandsWindows, FaBrandsLinux, FaBrandsApple } from "solid-icons/fa";
 export default function Index() {
   const links = [
     {
-      title: "Windows",
+      title: "Windows (x64)",
       icon: <FaBrandsWindows class="w-24 h-24 p-4" />,
-      link: "#",
+      link: "https://mc-launcher.r-artemev.ru/api/v1/file/win-x64-Gml.Launcher.exe",
     },
     {
-      title: "Linux",
+      title: "Windows (x86)",
+      icon: <FaBrandsWindows class="w-24 h-24 p-4" />,
+      link: "https://mc-launcher.r-artemev.ru/api/v1/file/win-x86-Gml.Launcher.exe",
+    },
+    {
+      title: "Linux (amd64)",
       icon: <FaBrandsLinux class="w-24 h-24 p-4" />,
-      link: "#",
+      link: "https://mc-launcher.r-artemev.ru/api/v1/file/linux-x64-Gml.Launcher",
     },
     {
-      title: "MacOS",
+      title: "MacOS (скоро, просите у @artemevkhv)",
       icon: <FaBrandsApple class="w-24 h-24 p-4" />,
       link: "#",
     },
@@ -22,7 +27,7 @@ export default function Index() {
 
   return (
     <div class="h-full flex flex-col items-center justify-center font-extralight">
-      <h1 class="text-4xl text-center">Добро пожаловать на Project Name</h1>
+      <h1 class="text-4xl text-center">Добро пожаловать на GupsShield</h1>
 
       <h2 class="mt-4 text-2xl text-center">Скачать лаунчер:</h2>
       <div class="mt-4 flex flex-wrap gap-4 items-center justify-center text-center">
