de62941f16f9ecfbb1b888d11621014b4adaca6f
Gru <78332542+gru2007@users.noreply.github.com>
2025-10-07 12:49:38 +1000

    Update index.jsx

--- packages/frontend/src/pages/index.jsx
+++ packages/frontend/src/pages/index.jsx
@@ -19,9 +19,9 @@ export default function Index() {
       link: "https://mc-launcher.r-artemev.ru/api/v1/file/linux-x64-Gml.Launcher",
     },
     {
-      title: "MacOS (Intel)",
+      title: "MacOS (Universal)",
       icon: <FaBrandsApple class="w-24 h-24 p-4" />,
-      link: "https://mc-launcher.r-artemev.ru/api/v1/file/osx-x64-Gml.Launcher",
+      link: "https://github.com/FETSU-Council/Gml.Launcher/releases/download/1.0.1.0/GupsShield.zip",
     },
   ];
 
